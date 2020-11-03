85f6c393cb704b5724dad8beae1013a854cddb4b
primarinovic <marinovic.priscila@gmail.com>
2020-11-03 14:25:49 -0300

    tipos de variaveis com n. decimais

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,19 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int)salario;
+		float pontoFlutuante = 3.14f; // f para indicar q é um float 
+		
+		System.out.println(valor + " O (int) forçou o double a se tornar inteiro");
+		
+		long numeroGrande = 32145698701L;
+		// short numeroPequeno = 2121;
+		// byte b = 127;
+		
+		
+
+	}
+
+}
