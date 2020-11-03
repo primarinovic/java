4d2d99489a4f2da2d6d81ad32d4bdb4f45272885
primarinovic <marinovic.priscila@gmail.com>
2020-11-03 14:39:48 -0300

    mais tipos de variaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,25 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a'; // char declara sempre com 'aspas simples' e usa apenas 1 caracter
+		System.out.println("A primeira vogal é: " + letra);
+		
+		char valor = 66;
+		
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "alura cursos online de tecnologia"; // string declara sempre com "aspas duplas"
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+		
+		
+
+	}
+
+}
