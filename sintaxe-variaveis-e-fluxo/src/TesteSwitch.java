f05d198e9407af1d002c9255931cf642eb743891
primarinovic <marinovic.priscila@gmail.com>
2020-11-04 14:48:43 -0300

    switch

---
+++ sintaxe-variaveis-e-fluxo/src/TesteSwitch.java
@@ -0,0 +1,55 @@
+
+public class TesteSwitch {
+
+	public static void main(String[] args) {
+		
+		int mesAtual = 11;
+		
+		switch (mesAtual) {
+		case 1: 
+			
+			System.out.println("Estamos em Janeiro");
+			break;
+		case 2 :
+			System.out.println("Estamos em Fevereiro");
+			break;
+		case 3 :
+			System.out.println("Estamos em Março");
+			break;
+		case 4 :
+			System.out.println("Estamos em Abril");
+			break;
+		case 5 :
+			System.out.println("Estamos em Maio");
+			break;
+		case 6 :
+			System.out.println("Estamos em Junho");
+			break;
+		case 7 :
+			System.out.println("Estamos em Julho");
+			break;
+		case 8 :
+			System.out.println("Estamos em Agosto");
+			break;
+		case 9 :
+			System.out.println("Estamos em Setembro");
+			break;
+		case 10 :
+			System.out.println("Estamos em Outubro");
+			break;
+		case 11 :
+			System.out.println("Estamos em Novembro");
+			break;
+		case 12 :
+			System.out.println("Estamos em Dezembro");
+			break;
+		default:
+			System.out.println("Mês inválido");
+			break;
+				
+		
+		}
+
+	}
+
+}
