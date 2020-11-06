150b8d67e1b82cb16c65dc8cf21b291133eed136
primarinovic <marinovic.priscila@gmail.com>
2020-11-06 16:02:11 -0300

    metodos

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -3,4 +3,26 @@ public class Conta {
 	int agencia = 42;
 	int numero;
 	String titular;
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+		
+		System.out.println("Valor depositado: " + valor);
+		System.out.println("Saldo atualizado: " + this.saldo);
+	}
+	
+	public void saca(double valor) {
+		this.saldo -= valor;
+		
+		System.out.println("Valor sacado: " + valor); 
+		System.out.println("Saldo atualizado: " + this.saldo);
+	}
+	
+	public void transfere(double valor) {
+		this.saldo -= valor;
+		
+		System.out.println("Valor transferido: " + valor); 
+		System.out.println("Saldo atualizado: " + this.saldo);
+	}
+	
 }
---
+++ bytebank/src/TestaMetodo.java
@@ -0,0 +1,16 @@
+
+public class TestaMetodo {
+
+	public static void main(String[] args) {
+		Conta contaDoBilbo = new Conta();
+		contaDoBilbo.saldo = 100;
+		
+		contaDoBilbo.deposita(50);
+		
+		
+		contaDoBilbo.saca(20);
+		
+
+	}
+
+}
