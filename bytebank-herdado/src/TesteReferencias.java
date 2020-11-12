e9ab9c48c4c66429941edb44f9ca7e27df6fa841
primarinovic <marinovic.priscila@gmail.com>
2020-11-12 17:35:18 -0300

    revendo herança e polimorfismo

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -13,11 +13,15 @@ public class TesteReferencias {
 		EditorVideo ev = new EditorVideo();
 		ev.setSalario(2500.0);
 		
+		Designer d = new Designer();
+		d.setSalario(2000.0);
+		
 		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f);
 		controle.registra(ev);
+		controle.registra(d);
 		
 		System.out.println(controle.getSoma());
 
