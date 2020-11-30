832719f1154cb7e9e7896bbcf7b972c5ded644f8
primarinovic <marinovic.priscila@gmail.com>
2020-11-30 17:56:59 -0300

    JAR

---
+++ bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,20 @@
+package br.com.alura.bytebank;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+
+	public static void main(String[] args) {
+		Conta c = new ContaCorrente(123, 321);
+		
+		c.deposita(320.50);
+		
+		System.out.println(c.getSaldo());
+		
+		c.deposita(200.10);
+		
+		System.out.println(c.getSaldo());
+	}
+
+}
